354e5bc738d12a350d9fade96ecadc583cca4480
Marshall483 <60525058+Marshall483@users.noreply.github.com>
2020-06-12 23:12:44 +0300

    initial

---
+++ 88 – 104/96 task.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Text;
+
+namespace Informatic
+{
+    class Program
+    {
+        static double FuncA(int k)
+        {
+            if (k.Equals(1)) return 1;
+            return 1.0/2 * (Math.Sqrt(FuncB(k - 1)) + 1.0/2 * Math.Sqrt(FuncA(k - 1)));
+        }
+
+        static double FuncB(int k)
+        {
+            if (k.Equals(1)) return 1;
+            return 2 * Math.Pow(FuncA(k - 1), 2) + FuncB(k - 1);
+        }
+        static void Main() 
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < 2) return;
+
+            double result = 0;
+            for (int i = 2; i < n; i++)
+                result += FuncA(i) * FuncB(i);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
