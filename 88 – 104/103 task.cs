a39f03fb07b09893188afc48703ddb551bc3f51f
Ilshat Mukhamediarov <ilshatmukh@gmail.com>
2020-06-14 12:13:40 +0300

    103 task added

---
+++ 88 – 104/103 task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var e = Convert.ToDouble(Console.ReadLine());
+            float y;
+            y = 0;
+            while(true)
+            {
+                if ((((y+1)/(y+2))-y)<e)
+                {
+                    Console.WriteLine((y+1)/(y+2));
+                    break;
+                }
+                y = (y + 1) / (y + 2);
+            }
+            
+           
+        }
+    }
+}
