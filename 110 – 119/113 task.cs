97d5f6d923355e719aad4e00de76e0a87d7b4813
Marshall483 <60525058+Marshall483@users.noreply.github.com>
2020-06-15 22:59:34 +0300

    Adding 113, 110, 376

---
+++ 110 – 119/113 task.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Temp
+{
+    class Program
+    {
+
+        static long IncorrectFac(int n) //113
+        {
+            int start = 1;
+            long result = 1;
+            if ((n % 2).Equals(1))
+                start = 1;
+            else start = 2;
+
+            while (start <= n)
+            {
+                result = result * start;
+                start += 2;
+            }
+            return result;
+        }
+        static void Main() 
+        {
+
+            IncorrectFac(7);//113
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ 373 – 396/376 task.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Temp
+{
+    class Program
+    {
+
+        static void GetMatrix(out int[,] input, int[] vector) //376
+        {
+            input = new int[vector.Length, vector.Length];
+
+            for (int i = 0; i < input.GetLength(0); i++, Console.WriteLine())
+                for (int j = 0; j < input.GetLength(1); j++)
+                { input[i, j] = vector[j]; Console.Write(input[i, j] + " "); }
+
+            Console.WriteLine();
+        }
+        static void MarkedString(int m) //376
+        {
+            int negative = 0;
+            int[,] matrix;
+            int[] vector = new int[m];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < m; i++)
+                vector[i] = rnd.Next(-20, 20);
+
+            GetMatrix(out matrix, vector);
+
+            for (int i = 0; i < vector.Length; i++)
+                if (vector[i] > 0) //Marced
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                        if (matrix[i, j] < 0) { negative++; matrix[i, j] = -1; }
+                        else if (matrix[i, j] > 0) matrix[i, j] = 1;
+
+            for (int i = 0; i < matrix.GetLength(0); i++, Console.WriteLine())
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write(matrix[i, j] + " ");
+
+            Console.WriteLine("Negative elements: " + negative);
+        }
+        static void Main() 
+        {
+
+            MarkedString(7);//376
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
