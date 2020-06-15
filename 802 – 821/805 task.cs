d90ea53c94d77cff0202a558bfbdb3cddb5e9d94
Ilshat Mukhamediarov <ilshatmukh@gmail.com>
2020-06-15 09:56:18 +0300

    805 task added

---
+++ 802 – 821/805 task.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _805
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder s = new StringBuilder(Console.ReadLine());
+            Regex r = new Regex(@"[a-z]"); 
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(r.IsMatch(s[i].ToString()))
+                {
+                    int j = i+1;
+                    try
+                    {
+                        while (r.IsMatch(s[j].ToString()))
+                        {
+                            j++;
+                        }
+                        int len = s.Length;
+                        s.Remove(j, s.Length - j);
+                        for (int k = j; k < len; k++)
+                        {
+                            s.Append('.');
+                        }
+                    }
+                    catch
+                    {
+                        
+                    }
+                    break;
+                }
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
