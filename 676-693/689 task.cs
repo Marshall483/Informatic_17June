3ee56bc9eb398c567fdb565ef35b03b6e28dfe54
Ilshat Mukhamediarov <ilshatmukh@gmail.com>
2020-06-15 10:50:18 +0300

    689 task added

---
+++ 676-693/689 task.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _689
+{
+    class Program
+    {
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("\t"+matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            const int n = 7;
+            const int m = 7;
+            int[,] matrix = new int[n, m];
+
+            int row = 0;
+            int col = 0;
+            int dx = 1;
+            int dy = 0;
+            int dirChanges = 0;
+            int visits = m;
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[row, col] = i + 1;
+                if (--visits == 0)
+                {
+                    visits = m * (dirChanges % 2) + n * ((dirChanges + 1) % 2) - (dirChanges / 2) - 1; 
+                    int temp = dx;
+                    dx = -dy;
+                    dy = temp;
+                    dirChanges++;
+                }
+
+                col += dx;
+                row += dy;
+            }
+            PrintMatrix(matrix);
+
+        }
+    }
+}
