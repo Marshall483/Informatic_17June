d0adc437de1bb0ba22d571f970998f866416fe7f
Marshall483 <60525058+Marshall483@users.noreply.github.com>
2020-06-16 21:48:31 +0300

    task 102 and 117 from Ildar

---
+++ 110 – 119/117 task.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecxercise117
+{
+    class Program
+    {
+        static double APoint(int n)
+        {
+            double num = 1;
+            int number = 1;
+            for (int i =0; i < n; i++)
+            {
+                num *= (double)number / (number + 1);
+                number += 2;
+            }
+            Console.WriteLine(num);
+            return num;
+        }
+        static double BPoint(int n)
+        {
+            double num = 1;
+            if (n == 1)
+            {
+                return num;
+            }
+            else
+            for (int i = 2; i <= n; i++)
+            {
+                    num *= (double)(2*i-1)/i;
+            }
+            return (num);
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            //Console.WriteLine(APoint(n));
+            //Console.WriteLine(BPoint(n));
+        }
+    }
+}
---
+++ 88 – 104/102 task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numb = 1;
+            double nextnumb = (2 - Math.Pow(numb, 3)) / 5;
+            int ind = 0;
+            while (Math.Abs(numb - nextnumb) > Math.Pow(10,-5))
+            {
+                ind++;
+                numb = nextnumb;
+                nextnumb = (2 - Math.Pow(numb, 3)) / 5;
+            }
+            Console.WriteLine(@"x{0}  =  {1}", ind, numb);
+        }
+    }
+}
