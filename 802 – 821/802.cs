b772f27140753d4557154dfc30b4061298af9f6c
SCVRLET <55560591+SCVRLET@users.noreply.github.com>
2020-06-16 22:09:32 +0300

    Makarov D

---
+++ 802 – 821/802.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 802
+
+            string s = Console.ReadLine();
+            int count = 0;
+            int max = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (Char.IsDigit(s[i]) == true)
+                {
+                    count++;
+                }
+                if (count > max)
+                {
+                    max = count;
+                }
+                if (Char.IsDigit(s[i]) == false)
+                {
+                    count = 0;
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
