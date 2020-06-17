fab559c20dc917f78c3d979520c33a29aefcca72
ByMyKolaps <55626010+ByMyKolaps@users.noreply.github.com>
2020-06-17 09:53:55 +0300

    Add files via upload

---
+++ 373 – 396/378.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _378
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 378 а)
+            double[] x = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            double[] y = { 1, 1, 1, 1, 1, 1, 1, 1 };
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    y[j] = y[j] * x[j];
+                    Console.Write(y[j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
