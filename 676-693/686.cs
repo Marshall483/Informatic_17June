f1b4dd310d76911f17e24699c49680ba22403e17
ByMyKolaps <55626010+ByMyKolaps@users.noreply.github.com>
2020-06-17 09:54:19 +0300

    Add files via upload

---
+++ 676-693/686.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _686
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 8;
+            int[,] result = new int[2 * n, 2 * n];
+            for (int i = 0; i < 2 * n; i++)
+            {
+                for (int j = 0; j < 2 * n; j++)
+                {
+                    if (i < n && j < n)     //     < <
+                    {
+                        result[i, j] = 1;
+                        continue;
+                    }
+                    if (i >= n && j < n)     //     > <
+                    {
+                        result[i, j] = 2;
+                        continue;
+                    }
+                    if (i < n && j >= n)     //     < > 
+                    {
+                        result[i, j] = 3;
+                        continue;
+                    }
+                    if (i >= n && j >= n)     //    > >
+                    {
+                        result[i, j] = 4;
+                        continue;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 2 * n; i++)
+            {
+                for (int j = 0; j < 2 * n; j++)
+                {
+                    Console.Write(result[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 676-693/693.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _693
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 693
+            int n = 2;
+            double[,] matrix = new double[2 * n, 2 * n];
+            double[,] result = new double[2 * n, 2 * n];
+            for (int i = 0; i < 2 * n; i++)
+                for (int j = 0; j < 2 * n; j++)
+                    matrix[i, j] = Convert.ToDouble(Console.ReadLine());
+            for (int i = 0; i < 2 * n; i++)
+            {
+                for (int j = 0; j < 2 * n; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            double temp = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[i + n, j + n];
+                    matrix[i + n, j + n] = temp;
+                }
+            }
+            for (int i = n; i < 2 * n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[j, i];
+                    matrix[j, i] = temp;
+                }
+            }
+            for (int i = 0; i < 2 * n; i++)
+            {
+                for (int j = 0; j < 2 * n; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
